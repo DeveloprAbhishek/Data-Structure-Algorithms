3986eee364bd2220c5d19e906b3d191ead13dc89
abhishek <developr.abhishek@gmail.com>
2022-08-24 18:44:05 +0530

    Solved pattern question

---
+++ src/pattern/PatternPrinting2.java
@@ -0,0 +1,36 @@
+package pattern;
+
+import java.util.Scanner;
+
+/*
+Pattern you have to print like as shown below.
+
+*       *
+*       *
+*       *
+*       *
+* * * * *
+ */
+
+public class PatternPrinting2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for (int i = 0; i < n; i++) {
+            if (i<n-1) {
+                for (int j = 0; j < n; j++) {
+                    if(j==0 || j==n-1) {
+                        System.out.print("* ");
+                    } else {
+                        System.out.print("  ");
+                    }
+                }
+            } else {
+                for (int j = 0; j < n; j++) {
+                    System.out.print("* ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
